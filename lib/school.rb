13a8222744e79a05ce9885f7f2520c1e65949936
nkusumo <nis.kusumo@gmail.com>
2021-06-30 13:33:48 -0400

    commit tree e3aff21468da62f8f34ea4b88f6d800677c404cd

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_reader :roster
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+      if @roster[grade]
+        @roster[grade] << name
+      else
+        @roster[grade] = [name]
+      end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+
+end
